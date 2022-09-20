1be514959828f0994b6b10724acaa20c4f59291d
1YanDevo <wadeyannick@gmail.com>
2022-09-20 01:19:17 -0500

    working with components

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -13,12 +13,27 @@ export class CoursesComponent implements OnInit {
       description: 'Learn the fundamentals of Angular 13',
       percentComplete: 26,
       favorite: true
+    },
+    {
+      id: 2,
+      title: 'Web Dev Essentials',
+      description: 'Learn the fundamentals of Web Development Now!',
+      percentComplete: 26,
+      favorite: true
     }
   ];
+
+
+  selectedCourse = null;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+
+
+  selectCourse(course) {
+    this.selectedCourse = course;
+  }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -17,7 +17,14 @@ export class HomeComponent implements OnInit {
     { title: 'Unit Testing Fundamentals' },
   ];
 
+  selectedLesson = null;
+
   constructor() {}
 
   ngOnInit() {}
+
+  selectLesson(lesson){
+    this.selectedLesson = lesson
+  }
+
 }
